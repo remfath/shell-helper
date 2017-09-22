01958801c6d26028ca08c0b5eac6df32e5b6da61
remfath <remfath@gmail.com>
2017-09-22 12:55:15 +0800

    Add format time function

--- lib/datetime.sh
+++ lib/datetime.sh
@@ -16,6 +16,50 @@ function getDateRangeAsc() {
   done
 }
 
+# Get hour part of a given second
+function getHourPart() {
+    local totalSeconds=$1
+    local hourPart=$((totalSeconds/3600))
+
+    echo $hourPart
+    return 0
+}
+
+# Get minute part of a given second
+function getMinutePart() {
+    local totalSeconds=$1
+    local hourPart=$(getHourPart "$totalSeconds")
+
+    totalSeconds=$((totalSeconds - 3600 * hourPart))
+    local minutePart=$((totalSeconds/60))
+
+    echo $minutePart
+    return 0
+}
+
+# Get second part of a given second
+function getSecondPart() {
+    local totalSeconds=$1
+    local hourPart=$(getHourPart "$totalSeconds")
+    local minutePart=$(getMinutePart "$totalSeconds")
+
+    local secondPart=$((totalSeconds-3600*hourPart-60*minutePart))
+
+    echo $secondPart
+    return 0
+}
+
+# Format a given second to <Hour>h<Minute>m<Second>s
+function formatTime() {
+    local totalSeconds=$1
+    local hourPart=$(getHourPart "${totalSeconds}")
+    local minutePart=$(getMinutePart "${totalSeconds}")
+    local secondPart=$(getSecondPart "${totalSeconds}")
+
+    echo "${hourPart}h{minutePart}m${secondPart}s"
+    return 0
+}
+
 
 
 ############################### Example ###############################
