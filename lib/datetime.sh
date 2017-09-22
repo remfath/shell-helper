654f2fece32b16dca5cea7dc64e0df6e6166a55c
remfath <remfath@gmail.com>
2017-09-22 13:02:14 +0800

    Set some variable of function as local

--- lib/datetime.sh
+++ lib/datetime.sh
@@ -2,9 +2,9 @@
 
 # Loop date between the given range by asc sort
 function getDateRangeAsc() {
-  rangeStart=$1
-  rangeEnd=$2
-  doSomething=$3
+  local rangeStart=$1
+  local rangeEnd=$2
+  local doSomething=$3
 
   rangeEnd=`date -d "+1 day $rangeEnd" +%Y%m%d`
   while [[ $rangeStart -lt $rangeEnd ]]
